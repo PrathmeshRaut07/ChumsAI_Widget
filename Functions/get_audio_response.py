61bcc788b7607bcd4a72f443c0b34ebd877bd6b8
PrathmeshRaut07 <prathmeshraut150904@gmail.com>
2025-02-19 14:06:45 +0530

    error for speech recognition fixed

--- Functions/get_audio_response.py
+++ Functions/get_audio_response.py
@@ -4,7 +4,12 @@ import os
 import wave
 from Functions.get_text_response import response_to_text
 import numpy as np
+import google.generativeai as genai
+from dotenv import load_dotenv
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "GOOGLE_API.json"
+model = genai.GenerativeModel('models/gemini-2.0-flash')
 def response_to_audio(audio_data):
     """
     Process audio data through speech-to-text, get a response, and convert back to audio
@@ -15,30 +20,36 @@ def response_to_audio(audio_data):
     Returns:
     bytes: Generated audio response in WAV format
     """
-    speech_client = speech_v1.SpeechClient()
+    # speech_client = speech_v1.SpeechClient()
     
-    config = speech_v1.RecognitionConfig(
-        encoding=speech_v1.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=44100,
-        language_code="en-US",
-    )
+    # config = speech_v1.RecognitionConfig(
+    #     encoding=speech_v1.RecognitionConfig.AudioEncoding.LINEAR16,
+    #     sample_rate_hertz=44100,
+    #     language_code="en-US",
+    # )
 
-    audio = speech_v1.RecognitionAudio(content=audio_data)
+    # audio = speech_v1.RecognitionAudio(content=audio_data)
 
-    response = speech_client.recognize(config=config, audio=audio)
+    # response = speech_client.recognize(config=config, audio=audio)
     
-    if not response.results:
-        return None
+    # if not response.results:
+    #     return None
     
-    transcribed_text = response.results[0].alternatives[0].transcript
-
-    response_text = response_to_text(transcribed_text)
+    # transcribed_text = response.results[0].alternatives[0].transcript
+    prompt="you are a helful assitant"
+    response = model.generate_content([
+                prompt,
+                {
+                    "mime_type": "audio/mp3",
+                    "data": audio_data
+                }
+            ])
     
 
     tts_client = texttospeech.TextToSpeechClient()
     
-
-    synthesis_input = texttospeech.SynthesisInput(text=response_text)
+    print(response.text)
+    synthesis_input = texttospeech.SynthesisInput(text=response.text)
     
  
     voice = texttospeech.VoiceSelectionParams(
